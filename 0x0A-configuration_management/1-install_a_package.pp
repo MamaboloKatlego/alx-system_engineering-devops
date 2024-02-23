39d968ea22ec3508ab376f9319e720151c3f769a
MamaboloKatlego <rabotaikatlego@gmail.com>
2024-02-23 08:12:53 -0800

    task 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/pup
-# Docs 
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+#install pupplet lint
+package { 'puppet-lint':
+  ensure   => '1.1.0',
+  provider => 'gem',
 }
