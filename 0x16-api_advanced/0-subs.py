21e8ae4c92ff305ee4f446154fd696adcdb09aee
MamaboloKatlego <132917857+MamaboloKatlego@users.noreply.github.com>
2024-04-09 20:45:30 +0200

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-"""
-Module Docs
-"""
-import requests
+# get subs
+from requests import get
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """
-    Function Docs
-    """
-    url = 'https://www.reddit.com'
-    header = {
-        'Accept': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-    }
-    response = requests.get('{}/r/{}/about/.json'.format(url, subreddit),
-                            headers=header,
-                            allow_redirects=False
-                            )
-    if response.status_code == 200:
-        return response.json()['data']['subscribers']
-    return 0
+    """subs"""
+    head = {'User-Agent': 'Dan Kazam'}
+    count = get('https://www.reddit.com/r/{}/about.json'.format(
+        subreddit), headers=head).json()
+    try:
+        return count.get('data').get('subscribers')
+    except:
+        return 0
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
