3eebc28a0c26e29db1b75054d6390128b436248a
MamaboloKatlego <132917857+MamaboloKatlego@users.noreply.github.com>
2024-04-10 13:07:07 +0200

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
-
+""" A module that queries the REDDIT API and returns the total
+number of subscribers in a given subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return 0
+    """function to return no. of users or zero on failure"""
+    subs = 0
+    headers = {'User-agent': 'kirimiBot/0.0.1'}
+    url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code == 200:
+        data = r.json().get('data')
+        if data:
+            subs = data.get('subscribers')
+    return subs
